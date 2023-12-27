478ce15ec06ac49e714552bd7ca3665af48b60e5
yuanningd <65219674+yuanningd@users.noreply.github.com>
2023-12-27 13:33:17 +1100

    UNI-136: add retry function

---
+++ retry-order-processing/app.ts
@@ -0,0 +1,49 @@
+import { SQSEvent, SQSHandler } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+
+const lambda = new AWS.Lambda();
+const s3 = new AWS.S3();
+
+const BUCKET_NAME = 'your-s3-bucket-name'; // Replace with your bucket name
+const PROCESS_ORDER_FUNCTION = 'ProcessOrderFunction';
+
+export const handler: SQSHandler = async (event) => {
+  const messageBody = JSON.stringify(event);
+  try {
+    await invokeLambdaFunction(messageBody);
+  } catch (error) {
+    console.log(`Error invoking ${PROCESS_ORDER_FUNCTION}:`, error);
+    await handleInvocationError(event, messageBody, error);
+  }
+};
+
+async function invokeLambdaFunction(messageBody: string) {
+  const params = {
+    FunctionName: PROCESS_ORDER_FUNCTION,
+    InvocationType: 'RequestResponse',
+    Payload: messageBody,
+  };
+
+  await lambda.invoke(params).promise();
+}
+
+async function handleInvocationError(event: SQSEvent, messageBody: string, error: unknown) {
+  const messageId = event.Records[0].messageId;
+  try {
+    await storeMessageInS3(messageId, messageBody);
+    console.log(`Message ${messageId} stored in S3 after error:`, error);
+  } catch (s3Error) {
+    console.error(`Error storing message ${messageId} in S3:`, s3Error);
+    throw s3Error;
+  }
+}
+
+async function storeMessageInS3(messageId: string, messageBody: string) {
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: `failed-messages/${messageId}.txt`,
+    Body: messageBody,
+  };
+
+  await s3.putObject(params).promise();
+}
